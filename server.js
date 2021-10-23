4be0b782c49b57719b42eab9eed718d40dcc40a3
Lance Bontrager <87950314+1ance-301@users.noreply.github.com>
2021-10-23 12:10:59 -0400

    html routes

--- server.js
+++ server.js
@@ -1,6 +1,38 @@
 const express = require('express');
+
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3001;
+const { notes } = require('./Develop/db/db.json')
+
+// notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+});
+
+// start page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
+
+// default for unknown path
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
+
+// // middleware
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // displays notes
+// app.get('/notes', (req,res) => {
+//     let results = notes;
+//     res.json(results);
+// });
+
+// app.post('/api/notes', (req, res) => {
+
+// });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`)
